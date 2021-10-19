0b9fcb77085409d148797ec23ce289385b378b47
Hulk <jeepchenup@foxmail.com>
2021-10-19 19:07:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -159,6 +159,7 @@ def getBeijinTime():
                 msg_mi += main(user_mi,passwd_mi,min_1, max_1)
                 #print(msg_mi)
             if a:
+               print('小米运动步数修改：' + msg_mi)
                push('【小米运动步数修改】', msg_mi)
                push_wx(msg_mi)
                run(msg_mi)
